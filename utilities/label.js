8411a32adcf95a59fc51822f06fe6ad0a64be969
Safwan <safwan9f@gmail.com>
2021-03-11 01:36:53 +0300

    Implement uploading labels to S3 and getting back the url

--- utilities/label.js
+++ utilities/label.js
@@ -1,37 +1,19 @@
 const PDFDocument = require('pdfkit');
 const { Base64Encode } = require('base64-stream');
 const fs = require('fs');
+const upload = require('./upload_attachments');
+const dotenv = require('dotenv');
+const aws = require('aws-sdk');
 
 const label = function (res, shipments) {
-  var doc = new PDFDocument();
-  
-  // doc.pipe(fs.createWriteStream('output.pdf'));
-  generatePDF(doc, shipments);
-
-  // doc.end();
-    
-  var finalString = ''; // contains the base64 string
-  var stream = doc.pipe(new Base64Encode());
-  
-  doc.end(); // will trigger the stream to end
-  
-  stream.on('data', function(chunk) {
-      finalString += chunk;
-  });
-  
-  stream.on('end', function() {
-      // the stream is at its end, so push the resulting base64 string to the response
-      // res.set('Content-Type', "application/octet-stream");
-      // res.set('Content-Disposition', "attachment");
-      // res.write(finalString, 'base64');
-      res.json(finalString);
-  });
+  uploadFile(res, shipments);
 }
 
 function generatePDF(doc, shipments) {
     for(let shipment of shipments) {
       generatePDFPage(doc, shipment);
-      doc.addPage();
+      if(shipments.indexOf(shipment) !== shipments.length - 1)
+        doc.addPage();
     }
 }
 
@@ -40,8 +22,8 @@ function generatePDFPage(doc, shipment) {
   doc.fill('#000').stroke();
   doc.fontSize(16);
   doc.text(`Tracking No.: ${shipment.tracking_number}`, 32, 32, {lineBreak: false} );
-  const path = '../images/barcode.jpg';
-  doc.image(path, 32, 50, {fit: [300, 100]})
+
+  doc.image('./barcode.jpg', 32, 50, {fit: [300, 100]})
   doc.fontSize(12);
 
   doc.text(`Shipped From:\n${shipment.ship_from.country}, ${shipment.ship_from.city}\n${shipment.ship_from.address1}\n${shipment.ship_from.address2 | ''}\n${shipment.ship_from.postal_code}`, 32, 186 );
@@ -52,7 +34,42 @@ function generatePDFPage(doc, shipment) {
   doc.fill('#000').stroke();
   doc.text("Weight: 3kg\nDimensions\nHeight: 67cm\nWidth: 40cm", 32, (12*8*2 + 16 + 186) + 16 );
 
-  doc.image(path, 150, (12*8*2 + 16 + 186) + 16, {fit: [100, 100]})
+  doc.image('./barcode.jpg', 150, (12*8*2 + 16 + 186) + 16, {fit: [100, 100]})
+}
+
+async function uploadFile(res, shipments) {
+    var thisPDF = new PDFDocument();
+    generatePDF(thisPDF, shipments);
+    let buffers = [];
+    thisPDF.on('data', buffers.push.bind(buffers));
+    thisPDF.end();
+    thisPDF.on('end', () => {
+      pdfData = Buffer.concat(buffers);
+      dotenv.config();
+      aws.config.update({
+      secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
+      accessKeyId: process.env.AWS_ACCESS_KEY_ID,
+      });
+      console.log("dd");
+      const s3 = new aws.S3();
+      const fileName = 'shipments_labels';
+      const s3Params = {
+        Bucket: process.env.S3_BUCKET,
+        Body: pdfData,
+        Key: (+new Date())+'-'+fileName,
+        ContentType : 'application/pdf',
+      };
+      s3.upload(s3Params, (err, data) => {
+        delete pdfData, thisPDF;
+        pdfData = null;
+        thisPDF = null;
+        if(err) res.stats(400).send('Error while getting the labels!');
+        
+        res.send(data.Location);
+      });
+    });
+    
 }
 
 module.exports = label;
+
